9a77accd56c84caa671983def5f70b8b27481f87
Fatih <Fatihulil@gmail.com>
2023-06-19 13:52:17 +0700

    feat: :sparkles: Add Prisma Orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'prisma/prisma.module';
 import appConfig from './configs/app.config';
 
 @Module({
   imports: [
+    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
       load: [appConfig],
